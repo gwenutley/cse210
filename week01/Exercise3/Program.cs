82ba17931cf892a377adede00d830a210d063b62
gwenutley <gwen.utley24@gmail.com>
2025-05-09 13:33:59 -0600

    exerces 3+4, add numbers, guess random numbers. loops and if statements

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        float userGuess = -1;
+        while (userGuess != magicNumber)
+        {
+            Console.WriteLine("What is your guess? ");
+            string guess = Console.ReadLine();
+            userGuess = float.Parse(guess);
+
+            if (userGuess != magicNumber)
+            {
+                if (userGuess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (userGuess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,44 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        int number = -1;
+        while (number != 0)
+        {
+            Console.WriteLine("Enter number: ");
+            string userNumber = Console.ReadLine();
+            number = int.Parse(userNumber);
+
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+        }
+
+        int sum = 0;
+        foreach (int item in numbers)
+        {
+            sum = sum + item;
+        }
+        Console.WriteLine($"The sum is: {sum}");
+
+        float average = (float)sum / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        int max = -5000;
+        foreach (int item in numbers)
+        {
+            if (item > max)
+            {
+                max = item;
+            }
+        }
+        Console.WriteLine($"The largest number is: {max}");
     }
 }
