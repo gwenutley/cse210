a8ab556e65dcbdd658f6810d094ebddef94a3616
gwenutley <gwen.utley24@gmail.com>
2025-05-09 13:52:38 -0600

    dexercise 5, functions, collect user information and display it.

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayMessage();
+
+        string name = PromptUserName();
+        int number = PromptUserNumber();
+
+        int squared = SquareNumber(number);
+        DisplayResult(squared, name);
+    }
+
+    static void DisplayMessage()
+    {
+        Console.WriteLine("Welcome to the program");
+    }
+
+    static string PromptUserName()
+    {
+        Console.WriteLine("What is your name? ");
+        string name = Console.ReadLine();
+        return name;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.WriteLine("What is your favorite whole number? ");
+        int number = int.Parse(Console.ReadLine());
+        return number;
+    }
+
+    static int SquareNumber(int number)
+    {
+        int square = number * number;
+        return square;
+    }
+
+    static void DisplayResult(int square, string name)
+    {
+        Console.WriteLine($"{name}, the square of your number is {square}.");
     }
 }
